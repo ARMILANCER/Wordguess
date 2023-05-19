b795ab7fe12f98cac8f682b68819012c6d06244a
FabioStabile <128687560+FabioStabile@users.noreply.github.com>
2023-05-19 09:16:57 +0200

    Update Menu.java

--- Menu.java
+++ Menu.java
@@ -49,9 +49,14 @@ public class Menu extends JFrame {
             }
             System.out.println( "rows: "+dimension[0]+" columns: "+dimension[1]);
             new GUI(file,Integer.parseInt(dimension[0]),Integer.parseInt(dimension[1]));
+            
+            ProgressBar bar = new ProgressBar(file, rows, line.length());
+        	bar.setVisible(true);
+            
             dispose();
         });
-
+        
+        buttonKey(btnStart, KeyEvent.VK_ENTER);
         add(btnStart);
 
         setTitle("Welcome to crossWord!");
@@ -62,6 +67,23 @@ public class Menu extends JFrame {
         setResizable(false);
         setVisible(true);
     }
+    public static void buttonKey(JButton btn, int key) {
+	    Action action = new AbstractAction() {
+			@Override
+	         public void actionPerformed(ActionEvent e) {
+	            btn.doClick();
+	         }
+	    };
+
+	    // String actionKey = "buttonAction" + key;
+	        
+	    // Set the btn actionCommand to the key to identify which action should be performed 
+	    btn.setActionCommand("buttonAction" + key);
+	       
+	    // Associate the action object to the keyboard key
+	    btn.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke(key, 0), "buttonAction" + key);
+	    btn.getActionMap().put("buttonAction" + key, action);
+	}
 
     public static void main(String[] args) {
         new Menu();
