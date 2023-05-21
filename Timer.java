2383e04f5a575919588dcbab34f0d6e8121a47a0
FabioStabile <128687560+FabioStabile@users.noreply.github.com>
2023-05-21 23:06:01 +0200

    Update Timer.java

--- Timer.java
+++ Timer.java
@@ -1,4 +1,4 @@
-import java.awt.Font;
+/*import java.awt.Font;
 import java.io.IOException;
 
 import javax.swing.*;
@@ -20,7 +20,7 @@ public class Timer extends JLabel implements Runnable{
 		super("0");
 		setFont(new Font("Arial", Font.BOLD, 20));
 		new Thread(this).start();
-	}*/
+	}
 	//fine
 	
 	
@@ -38,7 +38,7 @@ public class Timer extends JLabel implements Runnable{
 					gui.blockProgram(gui, 2);
 					
 				/*count++;
-				setText("" + count);*/
+				setText("" + count);
 				
 				//fine
 				try {
@@ -60,4 +60,60 @@ public class Timer extends JLabel implements Runnable{
 	}
 	//fine
 
+}*/
+	
+//contando che ho cambiato la maggior parte delle cose faccio copia e incolla e bona
+import java.awt.Font;
+import javax.swing.*;
+
+
+public class Timer extends JLabel implements Runnable{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private boolean stop = false;
+	private GUI gui;
+	private static final int maxMinutes = 10, maxSeconds = 60;
+	private int count = maxMinutes * maxSeconds, seconds, minutes;
+	private String time = "";
+	public Timer(GUI gui) {
+		super("10:00");
+		//super("" + maxTime);
+		setFont(new Font("Arial", Font.BOLD, 20));
+		new Thread(this).start();
+		this.gui = gui;
+	}
+
+	@Override
+	public void run() {
+		synchronized(this) {
+			while(!stop) {
+				if(count >= 0) {
+					minutes = count / maxSeconds;
+					seconds = count % maxSeconds;
+					
+					time = String.format("%02d:%02d", minutes, seconds);
+					
+					setText(time);
+					count--;
+				}else
+					gui.blockProgram(gui, 2);
+				try {
+					wait(1000);
+				}catch(InterruptedException e1) {
+					e1.printStackTrace();
+				}
+			}
+		}
+	}
+		
+	public synchronized void stop() {
+		stop = true;
+	}
+
+	public synchronized int getCount() {
+		return count;
+	}
 }
+	
